33d9f95ad7b00fdaf0addc9e548a6cd976e3b32a
Yusuf Kaan Ocak <lvlzyro@gmail.com>
2022-09-14 12:18:36 +0300

    trying os recognizer

--- main.go
+++ main.go
@@ -9,4 +9,12 @@ func main() {
 	os := runtime.GOOS
 	fmt.Println("this is the start point of manti.")
 	fmt.Println("you're using " + os)
+	switch os {
+	case "windows":
+		fmt.Println("you'll need .exe")
+	case "linux":
+		fmt.Println("you'll need .deb or .rpm")
+	case "darwin":
+		fmt.Println("you'll need .app")
+	}
 }
